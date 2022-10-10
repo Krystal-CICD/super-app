6d1c4c92e1378cd709ec3d53676d9d37f6eded19
Phuong Duy Nguyen <60810674+phuongnd96@users.noreply.github.com>
2022-10-10 21:57:57 +0700

    feat: update response

--- hello.go
+++ hello.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
+        fmt.Fprintf(w, "Welcome to my website !!!")
     })
     dt := time.Now()
       
