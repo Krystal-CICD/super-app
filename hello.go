bae7a51c926b3270903b0e5a9adb06ea48a6fe0b
Tung Pun <htung.nht@gmail.com>
2022-10-10 11:37:08 +0700

    Update hello.go

--- hello.go
+++ hello.go
@@ -3,12 +3,16 @@ package main
 import (
     "fmt"
     "net/http"
+    "time"
 )
 
 func main() {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Welcome to my website!")
     })
-
+    dt := time.Now()
+      
+    // printing the time in string format
+    fmt.Println("Current date and time is: ", dt.String())
     http.ListenAndServe(":8080", nil)
 }
